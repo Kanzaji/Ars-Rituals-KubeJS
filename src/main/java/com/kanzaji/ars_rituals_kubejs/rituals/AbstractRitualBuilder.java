f2a008f63f83d77ef714a84322b953a43717005f
Kanzaji <60540476+Kanzaji@users.noreply.github.com>
2024-05-29 06:58:30 +0200

    Another small batch of work

--- src/main/java/com/kanzaji/ars_rituals_kubejs/rituals/AbstractRitualBuilder.java
+++ src/main/java/com/kanzaji/ars_rituals_kubejs/rituals/AbstractRitualBuilder.java
@@ -1,37 +1,130 @@
 package com.kanzaji.ars_rituals_kubejs.rituals;
 
 import com.hollingsworth.arsnouveau.api.ritual.AbstractRitual;
+import com.hollingsworth.arsnouveau.client.particle.ParticleColor;
 import com.hollingsworth.arsnouveau.setup.registry.APIRegistry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.ApiStatus.Experimental;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Consumer;
-import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class AbstractRitualBuilder {
+    //TODO: Methods to add
+    // - canConsumeItem + Some ready-to-go impl (Server Only? I think)
+    // Stuff to do:
+    // - Generate localization files automatically.
+    // - Force this to be only avaiable in the Startup scripts.
+    // - Create your own event
+    // - Create few ready-to-go impl of few things
+    // > setConsumedItems.
     private final ResourceLocation id;
-    private int cost = 0;
-    private Consumer<AbstractRitual> serverTick = (r) -> {};
-    private Consumer<AbstractRitual> clientTick = (r) -> {};
+    private boolean canBeTraded = true;
+    private int sourceCost = 0;
+    private int particleIntensity = 10;
+    private ParticleColor outerColor = ParticleColor.WHITE;
+    private ParticleColor innerColor = ParticleColor.WHITE;
+    private Predicate<ItemStack> canConsume = (i) -> false;
+    private Predicate<AbstractRitual> canStartServer = (r) -> true;
+    private Predicate<AbstractRitual> canStartClient = (r) -> true;
     private Consumer<AbstractRitual> onServerStart = (r) -> {};
     private Consumer<AbstractRitual> onClientStart = (r) -> {};
-    private Function<AbstractRitual, Boolean> canStart = (r) -> true;
+    private Consumer<AbstractRitual> serverTick = (r) -> {};
+    private Consumer<AbstractRitual> clientTick = (r) -> {};
+    private Consumer<AbstractRitual> onServerEnd = (r) -> {};
+    private Consumer<AbstractRitual> onClientEnd = (r) -> {};
 
     private AbstractRitualBuilder(ResourceLocation id) {
         this.id = id;
     }
 
     @Contract("_ -> new")
-    public static @NotNull AbstractRitualBuilder create(String id) {
+    public static @NotNull AbstractRitualBuilder create(@NotNull String id) {
         try {
-            return new AbstractRitualBuilder(new ResourceLocation(id));
+            return new AbstractRitualBuilder(new ResourceLocation(Objects.requireNonNull(id, "ID Can't be null!")));
         } catch (Exception e) {
             throw new IllegalArgumentException("Incorrect ID provided! " + e.getMessage());
         }
+    }
+
+    public @NotNull AbstractRitualBuilder setCost(int sourceCost) {
+        this.sourceCost = sourceCost;
+        return this;
+    }
+
+    public @NotNull AbstractRitualBuilder canBeTraded(boolean value) {
+        this.canBeTraded = value;
+        return this;
+    }
+
+    public @NotNull AbstractRitualBuilder setParticleIntensity(int intensity) {
+        this.particleIntensity = intensity;
+        return this;
+    }
+
+    public @NotNull AbstractRitualBuilder setInnerColor(@NotNull ParticleColor color) {
+        this.innerColor = Objects.requireNonNull(color);
+        return this;
+    }
+
+    public @NotNull AbstractRitualBuilder setInnerColor(int r, int g, int b) {
+        if (r > 255 || g > 255 || b > 255) throw new IllegalArgumentException("Illegal value(s) passed as the argument. RGB Value can't be above 255! Ritual ID: " + id);
+        this.innerColor = new ParticleColor(r, g, b);
+        return this;
+    }
+
+    public @NotNull AbstractRitualBuilder setOuterColor(@NotNull ParticleColor color) {
+        this.outerColor = Objects.requireNonNull(color);
+        return this;
+    }
+
+    public @NotNull AbstractRitualBuilder setOuterColor(int r, int g, int b) {
+        if (r > 255 || g > 255 || b > 255) throw new IllegalArgumentException("Illegal value(s) passed as the argument. RGB Value can't be above 255! Ritual ID: " + id);
+        this.innerColor = new ParticleColor(r, g, b);
+        return this;
+    }
+
+    public @NotNull AbstractRitualBuilder setConsumePredicate(@NotNull Predicate<ItemStack> predicate) {
+        this.canConsume = Objects.requireNonNull(predicate);
+        return this;
+    }
+
+    /**
+     * Creates itemConsumptionPredicate that checks if item to be consumed is one of the provided items.
+     * @param items List of String IDs
+     * @return AbstractRitualBuilder
+     * @implNote NOT_IMPLEMENTED
+     */
+    public @NotNull AbstractRitualBuilder setConsumedItems(@NotNull List<String> items) {
+        return this;
+    }
+
+    @Experimental
+    public @NotNull AbstractRitualBuilder setStartPredicate(@NotNull Predicate<AbstractRitual> function) {
+        this.canStartServer = Objects.requireNonNull(function);
+        return this;
+    }
+
+    @Experimental
+    public @NotNull AbstractRitualBuilder setClientStartPredicate(@NotNull Predicate<AbstractRitual> function) {
+        this.canStartClient = Objects.requireNonNull(function);
+        return this;
+    }
+
+    public @NotNull AbstractRitualBuilder onRitualStart(@NotNull Consumer<AbstractRitual> action) {
+        this.onServerStart = Objects.requireNonNull(action);
+        return this;
+    }
+
+    public @NotNull AbstractRitualBuilder onClientRitualStart(@NotNull Consumer<AbstractRitual> action) {
+        this.onClientStart = Objects.requireNonNull(action);
+        return this;
     }
 
     public @NotNull AbstractRitualBuilder setTick(@NotNull Consumer<AbstractRitual> action) {
@@ -44,49 +137,57 @@ public class AbstractRitualBuilder {
         return this;
     }
 
-    public @NotNull AbstractRitualBuilder onStart(@NotNull Consumer<AbstractRitual> action) {
-        this.onServerStart = Objects.requireNonNull(action);
+    public @NotNull AbstractRitualBuilder onRitualEnd(@NotNull Consumer<AbstractRitual> action) {
+        this.onServerEnd = Objects.requireNonNull(action);
         return this;
     }
 
-    public @NotNull AbstractRitualBuilder onClientStart(@NotNull Consumer<AbstractRitual> action) {
-        this.onClientStart = Objects.requireNonNull(action);
-        return this;
-    }
-
-    public @NotNull AbstractRitualBuilder startCondition(@NotNull Function<AbstractRitual, Boolean> function) {
-        this.canStart = Objects.requireNonNull(function);
-        return this;
-    }
-
-    public @NotNull AbstractRitualBuilder setItemsToConsume(List<ItemStack> items) {
-        return this;
-    }
-
-    public @NotNull AbstractRitualBuilder setCost(int sourceCost) {
-        this.cost = sourceCost;
+    public @NotNull AbstractRitualBuilder onClientRitualEnd(@NotNull Consumer<AbstractRitual> action) {
+        this.onClientEnd = Objects.requireNonNull(action);
         return this;
     }
 
     /**
      * Build method that is used to register the new Ritual to the Ars Nouveau registry.
-     * If used with already existing ID, it will override it.
+     * @apiNote If used with already existing ID, it will override it.
      */
     public void build() {
-        APIRegistry.registerRitual(new AbstractRitualWrapper(id, cost, serverTick, clientTick, onServerStart, onClientStart));
+        build(id);
     }
 
     /**
      * Build method that allows to create a ritual based on this builder, but with different ID.
      * @param id ResourceLocation under which Ritual will be registered.
+     * @apiNote If used with already existing ID, it will override it.
      */
     public void build(@NotNull ResourceLocation id) {
-        APIRegistry.registerRitual(new AbstractRitualWrapper(Objects.requireNonNull(id), cost, serverTick, clientTick, onServerStart, onClientStart));
+        APIRegistry.registerRitual(new AbstractRitualWrapper(
+                Objects.requireNonNull(id),
+                canBeTraded,
+                sourceCost,
+                particleIntensity,
+                outerColor,
+                innerColor,
+                canConsume,
+                canStartServer,
+                canStartClient,
+                onServerStart,
+                onClientStart,
+                serverTick,
+                clientTick,
+                onServerEnd,
+                onClientEnd
+        ));
     }
 
+    /**
+     * Build method that allows to create a ritual based on this builder, but with different ID.
+     * @param id String representation of ResourceLocation under which Ritual will be registered.
+     * @apiNote If used with already existing ID, it will override it.
+     */
     public void build (@NotNull String id) {
         try {
-            this.build(new ResourceLocation(id));
+            this.build(new ResourceLocation(Objects.requireNonNull(id, "ID Can't be null!")));
         } catch (Exception e) {
             throw new IllegalArgumentException("Incorrect ID provided! " + e.getMessage());
         }
--- src/main/java/com/kanzaji/ars_rituals_kubejs/rituals/AbstractRitualWrapper.java
+++ src/main/java/com/kanzaji/ars_rituals_kubejs/rituals/AbstractRitualWrapper.java
@@ -1,41 +1,85 @@
 package com.kanzaji.ars_rituals_kubejs.rituals;
 
 import com.hollingsworth.arsnouveau.api.ritual.AbstractRitual;
+import com.hollingsworth.arsnouveau.client.particle.ParticleColor;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class AbstractRitualWrapper extends AbstractRitual {
     private final ResourceLocation id;
+    private final boolean canBeTraded;
+    private final int sourceCost;
+    private final int particleIntensity;
+    private final ParticleColor outerColor;
+    private final ParticleColor innerColor;
+    private final Predicate<ItemStack> canConsume;
+    private final Predicate<AbstractRitual> canStartServer;
+    private final Predicate<AbstractRitual> canStartClient;
     private final Consumer<AbstractRitual> onServerStart;
     private final Consumer<AbstractRitual> onClientStart;
     private final Consumer<AbstractRitual> serverTick;
     private final Consumer<AbstractRitual> clientTick;
-    private final int sourceCost;
+    private final Consumer<AbstractRitual> onServerEnd;
+    private final Consumer<AbstractRitual> onClientEnd;
     public AbstractRitualWrapper(
             ResourceLocation id,
+            boolean canBeTraded,
             int sourceCost,
-            Consumer<AbstractRitual> sTick,
-            Consumer<AbstractRitual> cTick,
+            int particleIntensity,
+            ParticleColor outerColor,
+            ParticleColor innerColor,
+            Predicate<ItemStack> canConsume,
+            Predicate<AbstractRitual> canStartServer,
+            Predicate<AbstractRitual> canStartClient,
             Consumer<AbstractRitual> onServerStart,
-            Consumer<AbstractRitual> onClientStart
+            Consumer<AbstractRitual> onClientStart,
+            Consumer<AbstractRitual> serverTick,
+            Consumer<AbstractRitual> clientTick,
+            Consumer<AbstractRitual> onServerEnd,
+            Consumer<AbstractRitual> onClientEnd
     ) {
-        this.id = id;
-        this.sourceCost = sourceCost;
-        this.serverTick = sTick;
-        this.clientTick = cTick;
-        this.onServerStart = onServerStart;
-        this.onClientStart = onClientStart;
+        this.id                 = id;
+        this.canBeTraded        = canBeTraded;
+        this.sourceCost         = sourceCost;
+        this.particleIntensity  = particleIntensity;
+        this.outerColor         = outerColor;
+        this.innerColor         = innerColor;
+        this.canConsume         = canConsume;
+        this.canStartServer     = canStartServer;
+        this.canStartClient     = canStartClient;
+        this.onServerStart      = onServerStart;
+        this.onClientStart      = onClientStart;
+        this.serverTick         = serverTick;
+        this.clientTick         = clientTick;
+        this.onServerEnd        = onServerEnd;
+        this.onClientEnd        = onClientEnd;
+    }
+
+    private AbstractRitualWrapper(@NotNull AbstractRitualWrapper ARW) {
+        this.id                 = ARW.id;
+        this.canBeTraded        = ARW.canBeTraded;
+        this.sourceCost         = ARW.sourceCost;
+        this.particleIntensity  = ARW.particleIntensity;
+        this.outerColor         = ARW.outerColor;
+        this.innerColor         = ARW.innerColor;
+        this.canConsume         = ARW.canConsume;
+        this.canStartServer     = ARW.canStartServer;
+        this.canStartClient     = ARW.canStartClient;
+        this.onServerStart      = ARW.onServerStart;
+        this.onClientStart      = ARW.onClientStart;
+        this.serverTick         = ARW.serverTick;
+        this.clientTick         = ARW.clientTick;
+        this.onServerEnd        = ARW.onServerEnd;
+        this.onClientEnd        = ARW.onClientEnd;
     }
 
     public AbstractRitualWrapper copy() {
-        return new AbstractRitualWrapper(this.id, this.sourceCost, this.serverTick, this.clientTick, this.onServerStart, this.onClientStart);
-    }
-
-    @Override
-    protected void tick() {
-        if (getLevel().isClientSide) serverTick.accept(this); else clientTick.accept(this);
+        return new AbstractRitualWrapper(this);
     }
 
     @Override
@@ -44,21 +88,36 @@ public class AbstractRitualWrapper extends AbstractRitual {
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        if (sourceCost > 0) this.setNeedsSource(true);
-        if (getLevel().isClientSide) onServerStart.accept(this); else onClientStart.accept(this);
-    }
-
-    @Override
-    public boolean canStart() {
-        return super.canStart();
-    }
-
-    @Override
     public int getSourceCost() {
         return this.sourceCost;
     }
+
+    @Override
+    public ParticleColor getCenterColor() {
+        return this.innerColor;
+    }
+
+    @Override
+    public ParticleColor getOuterColor() {
+        return this.outerColor;
+    }
+
+    @Override
+    public int getParticleIntensity() {
+        return this.particleIntensity;
+    }
+
+    @Override
+    public boolean canBeTraded() {
+        return this.canBeTraded;
+    }
+
+    @Override
+    public boolean canConsumeItem(ItemStack stack) {
+        return this.canConsume.test(stack);
+    }
+
+    // Wrapped functions for easier access to the original ones.
 
     /**
      * Convenient wrapper, as Ars Nouveau decided to name this getWorld()
@@ -67,4 +126,43 @@ public class AbstractRitualWrapper extends AbstractRitual {
     public Level getLevel() {
         return this.getWorld();
     }
+
+    public boolean isClient() {
+        return this.getLevel().isClientSide;
+    }
+
+    public boolean isServer() {
+        return !this.getLevel().isClientSide;
+    }
+
+    // Wrapped functions created by the user in KubeJS.
+
+    @Override
+    public boolean canStart() {
+        if (isServer()) return this.canStartServer.test(this); else return this.canStartClient.test(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (sourceCost > 0) this.setNeedsSource(true);
+        if (isServer()) onServerStart.accept(this); else onClientStart.accept(this);
+    }
+
+    @Override
+    protected void tick() {
+        if (isServer()) serverTick.accept(this); else clientTick.accept(this);
+    }
+
+    @Override
+    public void onEnd() {
+        super.onEnd();
+        if (isServer()) onServerEnd.accept(this); else onClientEnd.accept(this);
+    }
 }
+
+//TODO: Stuff to negotiate with users:
+// - Should all custom methods be wrapped in try-catch and reported in the logs instead of crashing the game?
+// - If 1st true -> Should tablet be voided or spawned above the brazer tile?
+// - If 1st true -> Should we mention it on the chat/to the player (somehow)?
+// - Should I add a text in RitualTablet#appnendHoverText to tell people the tablet is custom?
